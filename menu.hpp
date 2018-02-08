73aa4dfc23d59abc458587047a658db654057dc1
Xavier Hollingsworth <Xavierh93@hotmail.com>
2018-02-07 21:59:52 -0500

    Third commit: added menu.hpp

---
+++ menu.hpp
@@ -0,0 +1,16 @@
+//  menu.hpp
+//  groupProject
+//  This is the header file for the menu functions.
+
+
+#ifndef menu_hpp
+#define menu_hpp
+
+#include <stdio.h>
+
+
+void getValues(int*, int*, int*, int*, int*);
+
+bool playAgain(bool);
+
+#endif /* menu_hpp */
